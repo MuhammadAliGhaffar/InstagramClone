42602967ebf05d5b744394a2a3b597b8f5651e89
MuhammadAliGhaffar <alibrohi999@gmail.com>
2020-09-13 15:28:31 +0500

    Retrive data from parse server

--- app/src/main/java/com/example/instagramclone/SignUp.java
+++ app/src/main/java/com/example/instagramclone/SignUp.java
@@ -3,18 +3,25 @@ package com.example.instagramclone;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class SignUp extends AppCompatActivity {
 
     private EditText edt_id,edt_name,edt_father;
-
+    private TextView txtGetData;
+    private String allStudent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,23 @@ public class SignUp extends AppCompatActivity {
         edt_id=findViewById(R.id.edt_id);
         edt_name=findViewById(R.id.edt_name);
         edt_father=findViewById(R.id.edt_father);
+        txtGetData=findViewById(R.id.txtGetData);
+
+        txtGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseQuery<ParseObject> parseQuery= ParseQuery.getQuery("Student");
+                parseQuery.getInBackground("A7PQgzhZNU", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if(object !=null && e==null){
+                            txtGetData.setText(object.get("student_name")+"");
+                        }
+                    }
+                });
+            }
+        });
+
     }
 
 
@@ -53,6 +77,33 @@ public class SignUp extends AppCompatActivity {
         }
 
 
+    }
+
+    public void getAllData(View view){
+
+        allStudent="";
+
+        ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("Student");
+        queryAll.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e==null){
+                    if(objects.size() > 0){
+
+                        for(ParseObject student : objects){
+                            allStudent = allStudent + student.get("student_name") + student.get("student_father") + student.get("student_id") + "\n";
+
+                        }
+
+                        FancyToast.makeText(SignUp.this,allStudent,FancyToast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
+
+                    }else {
+                        FancyToast.makeText(SignUp.this,e.getMessage(),FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+
+                    }
+                }
+            }
+        });
     }
 
 }
