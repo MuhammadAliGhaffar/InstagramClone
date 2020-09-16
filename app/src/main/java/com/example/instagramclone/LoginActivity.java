0d7c882b4562a77d80e8fa197d736879be794329
MuhammadAliGhaffar <alibrohi999@gmail.com>
2020-09-16 14:38:51 +0500

    Adding splash screen and home activity and login sign up functionalities

--- app/src/main/java/com/example/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -1,14 +1,25 @@
 package com.example.instagramclone;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView txtSignUp;
+    private EditText edtLoginEmail,edtLoginPassword;
 
     private String n1,n2;
 
@@ -22,7 +33,69 @@ public class LoginActivity extends AppCompatActivity {
         n2=getColoredSpanned(" Sign Up.","#000");
         txtSignUp.setText(Html.fromHtml(n1+" "+n2));
 
+
+        edtLoginEmail=findViewById(R.id.edtLoginEmail);
+        edtLoginPassword=findViewById(R.id.edtLoginPassword);
+
     }
+
+    public void takeMeToSignUp(View view){
+        Intent intent=new Intent(LoginActivity.this,SignUpActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void loginIsPressed(View view){
+        ParseUser.logInInBackground(edtLoginEmail.getText().toString(), edtLoginPassword.getText().toString(), new LogInCallback() {
+            @Override
+            public void done(ParseUser parseuser, ParseException e) {
+                if(parseuser != null) {
+                    if(parseuser.getBoolean("emailVerified")){
+
+                        FancyToast.makeText(LoginActivity.this,"Welcome " + edtLoginEmail.getText().toString() + "!",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
+
+                        Intent intent =new Intent(LoginActivity.this,HomeActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+
+                    }else {
+
+                        ParseUser.logOut();
+                        FancyToast.makeText(LoginActivity.this,"Login Fail please verify your email first",FancyToast.LENGTH_LONG,FancyToast.ERROR,false).show();
+
+                    }
+                }else{
+
+                    ParseUser.logOut();
+                    FancyToast.makeText(LoginActivity.this,"Login Fail "+e.getMessage()+"please re-try",FancyToast.LENGTH_LONG,FancyToast.ERROR,false).show();
+
+                }
+            }
+        });
+    }
+
+
+    private void alertDisplayer(String title,String message,final boolean error){
+        AlertDialog.Builder builder =new AlertDialog.Builder(LoginActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        dialog.cancel();
+                        if(!error) {
+                            Intent intent =new Intent(LoginActivity.this,HomeActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        }
+
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
+
+    }
+
     private String getColoredSpanned(String text, String color) {
         String input = "<font color=" + color + ">" + text + "</font>";
         return input;
--- app/src/main/java/com/example/instagramclone/SignUpActivity.java
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.java
@@ -1,14 +1,85 @@
 package com.example.instagramclone;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private EditText edtSignUpUsername,edtSignUpEmail,edtSignUpPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        edtSignUpUsername=findViewById(R.id.edtSignUpUsername);
+        edtSignUpEmail=findViewById(R.id.edtSignUpEmail);
+        edtSignUpPassword=findViewById(R.id.edtSignUpPassword);
+
+    }
+    public void signupIsPressed(View view){
+        try{
+            //Sign up with parse
+            ParseUser user =new ParseUser();
+
+            user.setUsername(edtSignUpUsername.getText().toString());
+            user.setEmail(edtSignUpEmail.getText().toString());
+            user.setPassword(edtSignUpPassword.getText().toString());
+
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e==null){
+
+                        ParseUser.logOut();
+                        FancyToast.makeText(SignUpActivity.this,"Account Created Successfully please verify your email before Login",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
+
+                        Intent intent =new Intent(SignUpActivity.this,LoginActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+
+                    }
+                    else{
+
+                        FancyToast.makeText(SignUpActivity.this,"Error Account Creation failed account could not be created :"+e.getMessage(),FancyToast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
+
+                    }
+                }
+            });
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void alertDisplayer(String title,String message,final boolean error){
+        AlertDialog.Builder builder =new AlertDialog.Builder(SignUpActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        dialog.cancel();
+                        if(!error) {
+                            Intent intent =new Intent(SignUpActivity.this,LoginActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        }
+
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
+
     }
 }
